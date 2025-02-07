bf65f80eab60fed398980c436904c514a7ca88d0
kaisian <kaisian.tan@stengg.com>
2025-02-07 10:39:50 +0800

    cahnge variable from version to name

--- set-name.sh
+++ set-name.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 # Read the version from package.json
-VERSION=$(jq -r '.version' package.json)
+NAME=$(jq -r '.name' package.json)
 
 # Set the version as an environment variable
-echo "NEW_TAG=$VERSION" >> $GITHUB_ENV
+echo "NEW_NAME=$NAME" >> $GITHUB_ENV
